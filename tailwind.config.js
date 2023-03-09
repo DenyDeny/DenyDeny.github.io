9df2627d00c443fb556eb45c522b254c5749e100
Denis <medvedev.deny@mail.ru>
2023-03-09 21:33:49 +0000

    add cv

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'ptSans': ['PT Sans', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
